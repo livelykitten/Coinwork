44e91238432f5f667a475a36d4569ee3a15cd74f
livelykitten <58162598+livelykitten@users.noreply.github.com>
2021-04-25 17:58:13 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from cx_Freeze import setup, Executable
  
-buildOptions = dict(packages=['requests', 'time', 'threading', 'json', 'datetime', 'collections', 'ctypes', 'os', 'winsound'], excludes = ["tkinter", "numpy"])
+buildOptions = dict(packages=['requests', 'time', 'threading', 'json', 'datetime', 'collections', 'os', 'PySide6', 'pygame'], 
+excludes = ["tkinter", "numpy"],
+include_files = ['./alarm.wav'], include_msvcr=True)
  
-exe = [Executable('Document1.py')]
+exe = [Executable('main.py', base = "Win32GUI")]
  
 setup(
-    name='BitcoinWatcher',
+    name='CoinWatcher',
     version='0.0.1',
     author='Tony Min',
     description = 'description',
